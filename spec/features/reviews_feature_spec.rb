d95d58369192cd1a69dd4ebf4bfd3b0d147705b1
Eleanor Kavanagh-Brown <ekavanaghbrown@gmail.com>
2017-02-21 12:51:52 +0000

    Add working reviews function

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -9,7 +9,7 @@ require 'rails_helper'
       fill_in "Thoughts", with: "so so"
       select '3', from: 'Rating'
       click_button 'Leave Review'
-
+      
       expect(current_path).to eq '/restaurants'
       expect(page).to have_content('so so')
     end
