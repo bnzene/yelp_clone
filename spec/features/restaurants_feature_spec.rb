068f4705d27f7d9f47b824fc0e75ec5fa49a9ba7
Eleanor Kavanagh-Brown <ekavanaghbrown@gmail.com>
2017-02-21 12:16:59 +0000

    Went back to beginning of walkthrough 12

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'restatuants' do
+feature 'restaurants' do
   context 'no restaurants have been added' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
