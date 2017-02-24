a5a67c427dee4a2496038df8cba240d9168b8b65
Eleanor Kavanagh-Brown <ekavanaghbrown@gmail.com>
2017-02-24 15:10:53 +0000

    Extend ActiveRecord associations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,9 @@
 class Restaurant < ApplicationRecord # ActiveRecord::Base
   belongs_to :user
-  has_many :reviews, dependent: :destroy
-  validates :name, length: { minimum: 3 }, uniqueness: true
+  has_many :reviews,
+    -> { extending WithUserAssociationExtension },
+    dependent: :destroy
 
-  def build_review(attributes = {}, user)
-    attributes[:user] ||= user
-    reviews.build(attributes)
-  end
+  validates :name, length: { minimum: 3 }, uniqueness: true
 
 end
